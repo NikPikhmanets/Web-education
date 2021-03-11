66fad2306a9da01886d82ecfad82a0f7a6c84c66
Mykola Pikhmanets <nik.pikhmanets@gmail.com>
2021-03-11 16:45:38 +0200

    Task#29 On page, made earlier by PSD template, make the slides animate one position to the left or right by click on the arrows. The slider must be looped (infinite)
    Create simple slider for About Us section

--- src/js/main.js
+++ src/js/main.js
@@ -10,3 +10,29 @@ function openNav() {
         intro.style.display = "block";
     }
 }
+
+let slideIndex = 1;
+
+function nextSlide() {
+    showSlides(slideIndex++);
+}
+
+function prevSlide() {
+    showSlides(slideIndex--);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("about-items");
+
+    if (n > slides.length - 1) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+}
