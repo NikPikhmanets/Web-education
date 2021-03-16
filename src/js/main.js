0e75950114cedeef52b543e06a597355944d9340
Mykola Pikhmanets <nik.pikhmanets@gmail.com>
2021-03-16 12:04:28 +0200

    Task#29 On page, made earlier by PSD template, make the slides animate one position to the left or right by click on the arrows. The slider must be looped (infinite)
    Create simple jQuery plugin for slider

--- src/js/main.js
+++ src/js/main.js
@@ -2,4 +2,46 @@ $(document).ready(function () {
     $('.toggle-nav').on('click', function () {
         $('#h-nav').toggleClass('responsive');
     });
+    $('.slider').slider({
+        countElement: 4,
+        slideScroll: 1
+    });
 });
+
+(function ($) {
+        let slideIndex = 1;
+
+        function showSlides(slides, n) {
+            console.log('n:' + n)
+
+            if (n >= slides.length) {
+                slideIndex = 0
+            }
+            if (n < 0) {
+                slideIndex = slides.length - 1;
+            }
+            for (let i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";
+            }
+            console.log('slideIndex:' + slideIndex)
+            slides[slideIndex].style.display = "block";
+        }
+
+        function getSlides(slider) {
+            let slides = slider.find('.slide');
+            console.log('slides length:' + slides.length)
+            return slides;
+        }
+
+        $.fn.slider = function (params) {
+            let slider = $(this);
+            let slides = getSlides(slider);
+            slider.find('.slide-prev').on('click', function () {
+                showSlides(slides, ++slideIndex);
+            });
+            slider.find('.slide-next').on('click', function () {
+                showSlides(slides, --slideIndex);
+            });
+        }
+    }
+)(jQuery);
