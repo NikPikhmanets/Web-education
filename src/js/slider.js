dfd79035e0611527743841f6bb0dfb9b7c6bd00e
Mykola Pikhmanets <nik.pikhmanets@gmail.com>
2021-03-18 16:24:54 +0200

    Task#54 Fix jQuery plugin if represent one slide
    Fix by parameters for show set number of slides

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,171 +1,72 @@
 $(document).ready(function () {
     $('.slider').slider({
         countElement: 2,
-        slideScroll: 2
+        slideScroll: 1
     });
 });
 
 (function ($) {
         $.fn.slider = function (params) {
-            let sliderWrapper = $(this).find('.slider-wrapper'),
-                sliderItems = $(this).find('.slide'),
-                sliderControls = $(this).find('.arrows'),
-                sliderWrapperWidth,
-                itemWidth,
-                positionLeftItem = 0,
-                transform = 0,
-                step,
-                items = [],
-                interval = 0,
-                config = {
-                    isCycling: false,
-                    direction: 'right',
-                    interval: 5000,
-                    pause: true
-                };
+            let widthOfContainer = $(this).width() - 40,
+                sliderItem = $(this).children().children('.slide'),
+                sliderWrapper = $(this).children('.slider-wrapper'),
+                sliderControls = $(this).children('.arrows'),
+                widthOfItem,
+                sliderWrapperWidth;
 
-            for (let key in config) {
-                if (key in config) {
-                    config[key] = config[key];
-                }
+            function init() {
+                widthOfItem = widthOfContainer / params.countElement;
+                sliderItem.outerWidth(widthOfItem);
+                sliderWrapperWidth = sliderWrapper.width(widthOfItem * params.countElement);
+
+                console.log('widthOfItem:' + widthOfItem)
             }
 
-            for (let index = 0; index < sliderItems.length; index++) {
-                items.push({
-                    item: sliderItems[index],
-                    position: index,
-                    transform: 0
-                });
+            function nextSlide() {
+                sliderWrapper.animate(
+                    {'left': widthOfItem * params.slideScroll},
+                    moveNextCallback()
+                );
             }
 
-            let position = {
-                getItemMin: function () {
-                    let indexItem = 0;
-
-                    for (let index = 0; index < items.length; index++) {
-                        let item = items[index];
-
-                        if (item.position < items[indexItem].position) {
-                            indexItem = index;
-                        }
-                    }
-                    return indexItem;
-                },
-                getItemMax: function () {
-                    let indexItem = 0;
-
-                    for (let index = 0; index < items.length; index++) {
-                        let item = items[index];
-
-                        if (item.position > items[indexItem].position) {
-                            indexItem = index;
-                        }
-                    }
-                    return indexItem;
-                },
-                getMin: function () {
-                    return items[position.getItemMin()].position;
-                },
-                getMax: function () {
-                    return items[position.getItemMax()].position;
-                }
-            };
-
-            let transformItem = function (direction) {
-                let nextItem;
-
-                if (direction === 'right') {
-                    for (let i = 0; i < params.slideScroll; i++) {
-                        positionLeftItem++;
-                        let number = positionLeftItem + sliderWrapperWidth / itemWidth - 1;
-                        let max = position.getMax();
-
-                        if (number > max - params.countElement) {
-                            nextItem = position.getItemMin();
-                            items[nextItem].position = position.getMax() + 1;
-                            items[nextItem].transform += items.length * 100;
-                            items[nextItem].item.style.transform = 'translateX(' + items[nextItem].transform + '%)';
-                        }
-                    }
-                    transform -= step;
-                }
-                if (direction === 'left') {
-                    for (let i = 0; i < params.slideScroll; i++) {
-                        positionLeftItem--;
-                        let min = position.getMin();
-
-                        if (positionLeftItem < min) {
-                            nextItem = position.getItemMax();
-                            items[nextItem].position = position.getMin() - 1;
-                            items[nextItem].transform -= items.length * 100;
-                            items[nextItem].item.style.transform = 'translateX(' + items[nextItem].transform + '%)';
-                        }
-                    }
-                    transform += step;
-                }
-                sliderWrapper.css('transform', 'translateX(' + (transform * params.slideScroll) + '%)');
-
-            };
-
-            // let cycle = function (direction) {
-            //     if (!config.isCycling) {
-            //         return;
-            //     }
-            //     interval = setInterval(function () {
-            //         initValueOfSize();
-            //         transformItem(direction);
-            //     }, config.interval);
-            // };
-
-            function getMargin(name) {
-                return parseInt($(sliderItems[0]).css(name));
+            function moveNextCallback() {
+                sliderWrapper.children(':nth-child(-n+' + params.slideScroll + ')').appendTo(sliderWrapper);
+                sliderWrapper.css('left', 0);
             }
 
-            function initValueOfSize() {
-                sliderWrapperWidth = $(sliderWrapper).width();
-                itemWidth = sliderItems[0].clientWidth +
-                    getMargin('marginLeft') + getMargin('marginRight');
-                step = itemWidth / sliderWrapperWidth * 100;
+            function prevSlide() {
+                sliderWrapper.animate(
+                    {'left': widthOfItem * params.slideScroll},
+                    10,
+                    0,
+                    movePrevCallback()
+                );
+            }
+
+            function movePrevCallback() {
+                sliderWrapper.children(':nth-last-child(-n+' + params.slideScroll + ')').prependTo(sliderWrapper);
+                sliderWrapper.css('left', 0);
             }
 
             let controlClick = function (e) {
-                initValueOfSize();
-
-                if (e.target.classList.contains('arrows')) {
+                if (e.target.classList.contains('arrow-left')) {
                     e.preventDefault();
-                    let direction = e.target.classList.contains('slide-right') ? 'right' : 'left';
-                    transformItem(direction);
-                    // clearInterval(interval);
-                    // cycle(config.direction);
+                    prevSlide();
+                }
+                if (e.target.classList.contains('arrow-right')) {
+                    e.preventDefault();
+                    nextSlide();
                 }
             };
 
-            let setUpListeners = function () {
+            let setupListeners = function () {
                 for (const sliderControl of sliderControls) {
                     sliderControl.addEventListener('click', controlClick);
                 }
             };
 
-            setUpListeners();
-            // cycle(config.direction);
-
-            return {
-                right: function () {
-                    transformItem('right');
-                },
-                left: function () {
-                    transformItem('left');
-                },
-                stop: function () {
-                    config.isCycling = false;
-                    clearInterval(interval);
-                },
-                // cycle: function () {
-                //     config.isCycling = true;
-                //     clearInterval(interval);
-                //     cycle();
-                // }
-            }
+            init();
+            setupListeners();
         }
     }
 )(jQuery);
