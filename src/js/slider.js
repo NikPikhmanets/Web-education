740831f4bfd32729c89b5016108c614c02ad15f3
Mykola Pikhmanets <nik.pikhmanets@gmail.com>
2021-03-19 15:46:29 +0200

    Task#59 Fix animation of slider
    Fix animation by set  param of number of view  elements

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $('.slider').slider({
-        countElement: 4,
-        slideScroll: 1
+        countElement: 3,
+        slideScroll: 2
     });
 
     // $('.brands').slider({
@@ -29,39 +29,40 @@ $(document).ready(function () {
 
             function init() {
                 widthOfItem = slideItemWidth;
-                container.css('height', slideItemHeight);
+                container.css('height', slideItemHeight + 100);
                 sliderItemsContainer.css('width', slideItemWidth * slideItemCount);
                 sliderItems.css('width', slideItemWidth);
-                // sliderItemsContainer.children(':nth-child(-n+' + options.slideScroll + ')').appendTo(sliderItemsContainer);
-                // sliderItemsContainer.children(':nth-last-child(-n+' + options.slideScroll + ')').prependTo(sliderItemsContainer);
 
                 setupListeners();
             }
 
             function nextSlide() {
-                // sliderItemsContainer.children(':nth-child(-n+' + options.slideScroll + ')').appendTo(sliderItemsContainer);
-
                 sliderItemsContainer.animate(
-                    // {'left': widthOfItem * options.slideScroll},
+                    {'left': 0},
+                    500,
                     moveNextCallback()
                 );
+
             }
 
             function moveNextCallback() {
-                sliderItemsContainer.children(':nth-child(-n+' + options.slideScroll + ')').appendTo(sliderItemsContainer);
-                // sliderItemsContainer.css('left',);
+                sliderItemsContainer.children(':nth-last-child(-n+' + options.slideScroll + ')').prependTo(sliderItemsContainer);
+                sliderItemsContainer.css('left', -widthOfItem * options.slideScroll);
             }
 
             function prevSlide() {
                 sliderItemsContainer.animate(
-                    // {'left': widthOfItem * options.slideScroll},
+                    {'left': -widthOfItem * options.slideScroll},
+                    // {'left': 0},// if 4
+                    500,
                     movePrevCallback()
                 );
             }
 
             function movePrevCallback() {
-                sliderItemsContainer.children(':nth-last-child(-n+' + options.slideScroll + ')').prependTo(sliderItemsContainer);
-                // sliderItemsContainer.css('right', 0);
+                sliderItemsContainer.children(':nth-child(-n+' + options.slideScroll + ')').appendTo(sliderItemsContainer);
+                // sliderItemsContainer.css('left', widthOfItem * options.slideScroll);
+                sliderItemsContainer.css('left', 0);
             }
 
             let controlClick = function (e) {
