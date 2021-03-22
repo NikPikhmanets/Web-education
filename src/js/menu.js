478ec3fe68da9f5c5b60caf132c7a49a5579f528
Mykola Pikhmanets <nik.pikhmanets@gmail.com>
2021-03-22 16:05:05 +0200

    Task#60 Bind control another slider by "dots"
    Create number of indicators by size page, action by click on indicator

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
     $('.toggle-nav').on('click', function () {
         $('#h-nav').toggleClass('responsive');
+        $('.body').toggleClass('lock-scroll');
     });
 });
--- src/js/slider.js
+++ src/js/slider.js
@@ -1,112 +1,163 @@
 $(document).ready(function () {
-    // $(window).resize(function () {
-
-    let count = 4;
-
-    // if ($(this).outerWidth() <= 800 && $(this).outerWidth() > 600) {
-    //     count = 2;
-    // } else if ($(this).outerWidth() <= 600) {
-    //     count = 1;
-    // }
-    console.log('count:' + count);
-
     $('.slider').slider({
-        countElement: count,
+        countElement: 2,
         slideScroll: 1
     });
-    // });
-    // $('.brands-container').slider();
+
+    $('.brands-container').slider();
 });
 
 (function ($) {
         $.fn.slider = function (params) {
             let options = $.extend({
-                countElement: 5,
+                countElement: 3,
                 slideScroll: 1
             }, params);
 
-            // function main(t) {
-            let t = $(this);
-            let container = t,
-                sliderItemsContainer = t.children().first(),
-                sliderItems = sliderItemsContainer.children(),
-                slideItemCount = sliderItemsContainer.children().length,
-                sliderControls = t.children('.arrows'),
-                indicatorControls = t.children('.indicators'),
-                widthOfItem,
-                dots = [];
+            function main(t) {
+                let container = t,
+                    sliderItemsContainer = t.children().first(),
+                    sliderItems = sliderItemsContainer.children(),
+                    sliderItemsCount = sliderItemsContainer.children().length,
+                    sliderControls = t.children('.arrows'),
+                    indicatorControls = t.children('.indicators'),
+                    widthOfItem,
+                    currentIndicator,
+                    currentNumbIndicator,
+                    indicatorsCount;
 
-            function init() {
-                let widthOfContainer = t.width();
-                let slideItemWidth = widthOfContainer / options.countElement;
-                let slideItemHeight = sliderItems[0].clientHeight;
+                function getCountElement() {
+                    let win = $(this);
 
-                if (indicatorControls.length === 0) {
-                    container.css('height', slideItemHeight + 50);
-                }
-                sliderItemsContainer.css('width', slideItemWidth * slideItemCount);
-                sliderItems.css('width', slideItemWidth);
-
-
-                console.log('slideItemWidth:' + slideItemWidth)
-                console.log('options.countElement' + options.countElement)
-            }
-
-            function nextSlide() {
-                sliderItemsContainer.animate(
-                    {'left': 0},
-                    500,
-                    moveNextCallback()
-                );
-            }
-
-            function moveNextCallback() {
-                sliderItemsContainer.children(':nth-last-child(-n+' + options.slideScroll + ')').prependTo(sliderItemsContainer);
-                sliderItemsContainer.css('left', -widthOfItem * options.slideScroll);
-            }
-
-            function prevSlide() {
-                sliderItemsContainer.animate(
-                    {'left': -widthOfItem * options.slideScroll},
-                    // {'left': 0},// if countElement: 4
-                    500,
-                    movePrevCallback()
-                );
-            }
-
-            function movePrevCallback() {
-                sliderItemsContainer.children(':nth-child(-n+' + options.slideScroll + ')').appendTo(sliderItemsContainer);
-                // sliderItemsContainer.css('left', widthOfItem * options.slideScroll); // if countElement: 4
-                sliderItemsContainer.css('left', 0);
-            }
-
-            let controlClick = function (e) {
-                if (e.target.classList.contains('arrow-left')) {
-                    e.preventDefault();
-                    prevSlide();
-                }
-                if (e.target.classList.contains('arrow-right')) {
-                    e.preventDefault();
-                    nextSlide();
-                }
-            };
-
-            function setupListeners() {
-                for (const sliderControl of sliderControls) {
-                    sliderControl.addEventListener('click', controlClick);
+                    if (win.width() <= 600) {
+                        return 2;
+                    }
+                    if (win.width() <= 800) {
+                        return 3;
+                    }
+                    return 5;
                 }
 
+                function init() {
+                    let countElement = options.countElement;
+                    let widthOfContainer = t.width();
+                    let slideItemWidth = widthOfContainer / countElement;
+                    let slideItemHeight = sliderItems[0].clientHeight;
 
+                    if (indicatorControls.length === 0) {
+                        container.css('height', slideItemHeight + 50);
+                    } else {
+                        slideItemWidth = widthOfContainer / getCountElement();
+                        indicatorsCount = sliderItemsCount / getCountElement();
+                        $(indicatorControls).empty();
+
+                        for (let i = 0; i < indicatorsCount; i++) {
+                            const indicator = document.createElement("div");
+                            indicator.classList.add('indicator');
+                            $(indicatorControls).append(indicator);
+                            indicator.addEventListener("click", indicatorClick.bind(null, i), false);
+                        }
+                        currentIndicator = $(indicatorControls).children().first();
+                        setActiveIndicator(0);
+                    }
+                    sliderItemsContainer.css('width', slideItemWidth * sliderItemsCount);
+                    sliderItems.css('width', slideItemWidth);
+                }
+
+                function nextSlide() {
+                    sliderItemsContainer.animate(
+                        {'left': 0},
+                        500,
+                        moveNextCallback()
+                    );
+                }
+
+                function moveNextCallback() {
+                    sliderItemsContainer.children(':nth-last-child(-n+' + options.slideScroll + ')').prependTo(sliderItemsContainer);
+                    sliderItemsContainer.css('left', -widthOfItem * options.slideScroll);
+                }
+
+                function prevSlide() {
+                    sliderItemsContainer.animate(
+                        {'left': -widthOfItem * options.slideScroll},
+                        // {'left': 0},// if countElement: 4
+                        500,
+                        movePrevCallback()
+                    );
+                }
+
+                function movePrevCallback() {
+                    sliderItemsContainer.children(':nth-child(-n+' + options.slideScroll + ')').appendTo(sliderItemsContainer);
+                    // sliderItemsContainer.css('left', widthOfItem * options.slideScroll); // if countElement: 4
+                    sliderItemsContainer.css('left', 0);
+                }
+
+                let controlClick = function (e) {
+                    if (e.target.classList.contains('arrow-left')) {
+                        e.preventDefault();
+                        prevSlide();
+                    }
+                    if (e.target.classList.contains('arrow-right')) {
+                        e.preventDefault();
+                        nextSlide();
+                    }
+                };
+
+                function setActiveIndicator(num) {
+                    currentNumbIndicator = num;
+                    currentIndicator.addClass('active-indicator');
+                }
+
+                function indicatorClick(num) {
+                    if (num === currentNumbIndicator) {
+                        return;
+                    }
+                    console.log('indicator click: ' + num);
+
+                    if (num > currentNumbIndicator) {
+                        nextSlide();
+                    } else {
+                        prevSlide();
+                    }
+                    currentIndicator.removeClass('active-indicator');
+                    currentIndicator = $(indicatorControls).children().eq(num);
+                    setActiveIndicator(num);
+                }
+
+                function setupListeners() {
+                    for (const sliderControl of sliderControls) {
+                        sliderControl.addEventListener('click', controlClick);
+                    }
+                }
+
+                init();
+                setupListeners();
+
+
+                let waitForFinalEvent = (function () {
+                    let timers = {};
+                    return function (callback, ms, uniqueId) {
+                        if (!uniqueId) {
+                            uniqueId = "Don't call this twice without a uniqueId";
+                        }
+                        if (timers[uniqueId]) {
+                            clearTimeout(timers[uniqueId]);
+                        }
+                        timers[uniqueId] = setTimeout(callback, ms);
+                    };
+                })();
+
+                $(window).resize(function () {
+                    waitForFinalEvent(function () {
+                        init();
+                    }, 500, "some unique string");
+                });
             }
 
-            init();
-            setupListeners();
-            // }
-
-            // this.each(function () {
-            //     main($(this));
-            // });
-            // return this;
+            this.each(function () {
+                main($(this));
+            });
+            return this;
         }
     }
 )(jQuery);
