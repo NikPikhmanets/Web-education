10cab033b70feebc7550c203b5099dca6f32de7e
Mykola Pikhmanets <nik.pikhmanets@gmail.com>
2021-03-22 18:14:52 +0200

    Task#61 Sort items by category in Latest Works section the page
    Add function sort

--- src/js/sort.js
+++ src/js/sort.js
@@ -1,20 +1,35 @@
 $(document).ready(function () {
-    $('.latest-works').sorting();
+    $('.latest-works').sorting({
+        controls: '.lw-controls',
+        content: '.lw-item',
+        active: 'active-button',
+        category: ['all', 'category-b', 'category-des', 'category-dev', 'category-s']
+    });
 });
 
 (function ($) {
         const firstButton = 0;
+        const click = 'click';
 
         $.fn.sorting = function (params) {
+            let options = $.extend({
+                controls: params.controls,
+                content: params.content,
+                active: params.active,
+                category: params.category
+            });
+
+
             function main(t) {
-                let buttons = t.children('.lw-controls').children(),
+                let buttons = t.children(options.controls).children(),
                     currentNumbOfButton,
-                    currentButton;
+                    currentButton,
+                    items = t.find(options.content);
 
                 function initButtons() {
                     for (let i = 0; i < buttons.length; i++) {
                         let btn = buttons[i];
-                        btn.addEventListener("click", buttonsClick.bind(null, i))
+                        btn.addEventListener(click, buttonsClick.bind(null, i, options.category[i]))
                     }
                     currentButton = buttons.first();
                     setActiveButton(firstButton);
@@ -22,16 +37,32 @@ $(document).ready(function () {
 
                 function setActiveButton(num) {
                     currentNumbOfButton = num;
-                    currentButton.addClass('active-button');
+                    currentButton.addClass(options.active);
                 }
 
-                function buttonsClick(num) {
+                // function sort(c) {
+                //     if (c === "all") {
+                //         c = "";
+                //     }
+                //     for (let i = 0; i < items.length; i++) {
+                //         $(items[i]).removeClass('show');
+                //
+                //         if ( $(items[i]).hasClass(c) > -1) {
+                //             $(items[i]).addClass('show');
+                //         }
+                //     }
+                // }
+
+                function buttonsClick(num, category) {
+                    console.log(category);
+
                     if (num === currentNumbOfButton) {
                         return;
                     }
-                    currentButton.removeClass('active-button');
+                    currentButton.removeClass(options.active);
                     currentButton = $(buttons).eq(num);
                     setActiveButton(num);
+                    sort(category);
                 }
 
                 initButtons();
