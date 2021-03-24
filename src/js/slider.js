1542af9c5f6bb0f2786200227cb47f2bb59f5056
Mykola Pikhmanets <nik.pikhmanets@gmail.com>
2021-03-24 09:22:57 +0200

    Task#62 Fix animation in slider after refactoring  plugin
    Fix change for test

--- src/js/slider.js
+++ src/js/slider.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
         slideScroll: 1
     });
 
-    $('.brands-container').slider();
+    // $('.brands-container').slider();
 });
 
 (function ($) {
@@ -14,150 +14,151 @@ $(document).ready(function () {
                 slideScroll: 1
             }, params);
 
-            function main(t) {
-                let container = t,
-                    sliderItemsContainer = t.children().first(),
-                    sliderItems = sliderItemsContainer.children(),
-                    sliderItemsCount = sliderItemsContainer.children().length,
-                    sliderControls = t.children('.arrows'),
-                    indicatorControls = t.children('.indicators'),
-                    widthOfItem,
-                    currentIndicator,
-                    currentNumbIndicator,
-                    indicatorsCount;
+            // function main(t) {
+            //     let container = t,
+            let container = $(this),
+                sliderItemsContainer = container.children().first(),
+                sliderItems = sliderItemsContainer.children(),
+                sliderItemsCount = sliderItemsContainer.children().length,
+                sliderControls = container.children('.arrows'),
+                indicatorControls = container.children('.indicators'),
+                widthOfItem,
+                currentIndicator,
+                currentNumbIndicator,
+                indicatorsCount;
 
-                function getCountElementBySizeWin() {
-                    let win = $(this);
+            function getCountElementBySizeWin() {
+                let win = $(this);
 
-                    if (win.width() <= 600) {
-                        return 2;
-                    }
-                    if (win.width() <= 800) {
-                        return 3;
-                    }
-                    return 5;
+                if (win.width() <= 600) {
+                    return 2;
                 }
-
-                function init() {
-                    let countElement = options.countElement;
-                    let widthOfContainer = t.width();
-                    let slideItemWidth = widthOfContainer / countElement;
-                    let slideItemHeight = sliderItems[0].clientHeight;
-
-                    if (indicatorControls.length === 0) {
-                        container.css('height', slideItemHeight + 100);
-                    } else {
-                        slideItemWidth = widthOfContainer / getCountElementBySizeWin();
-                        indicatorsCount = sliderItemsCount / getCountElementBySizeWin();
-                        $(indicatorControls).empty();
-
-                        for (let i = 0; i < indicatorsCount; i++) {
-                            const indicator = document.createElement("div");
-                            indicator.classList.add('indicator');
-                            $(indicatorControls).append(indicator);
-                            indicator.addEventListener("click", indicatorClick.bind(null, i), false);
-                        }
-                        currentIndicator = $(indicatorControls).children().first();
-                        setActiveIndicator(0);
-                    }
-                    sliderItemsContainer.css('width', slideItemWidth * sliderItemsCount);
-                    sliderItems.css('width', slideItemWidth);
+                if (win.width() <= 800) {
+                    return 3;
                 }
-
-                function nextSlide() {
-                    sliderItemsContainer.animate(
-                        {'left': 0},
-                        500,
-                        moveNextCallback()
-                    );
-                }
-
-                function moveNextCallback() {
-                    sliderItemsContainer.children(':nth-last-child(-n+' + options.slideScroll + ')').prependTo(sliderItemsContainer);
-                    sliderItemsContainer.css('left', -widthOfItem * options.slideScroll);
-                }
-
-                function prevSlide() {
-                    sliderItemsContainer.animate(
-                        {'left': -widthOfItem * options.slideScroll},
-                        // {'left': 0},// if countElement: 4
-                        500,
-                        movePrevCallback()
-                    );
-                }
-
-                function movePrevCallback() {
-                    sliderItemsContainer.children(':nth-child(-n+' + options.slideScroll + ')').appendTo(sliderItemsContainer);
-                    // sliderItemsContainer.css('left', widthOfItem * options.slideScroll); // if countElement: 4
-                    sliderItemsContainer.css('left', 0);
-                }
-
-                let controlClick = function (e) {
-                    if (e.target.classList.contains('arrow-left')) {
-                        e.preventDefault();
-                        prevSlide();
-                    }
-                    if (e.target.classList.contains('arrow-right')) {
-                        e.preventDefault();
-                        nextSlide();
-                    }
-                };
-
-                function setActiveIndicator(num) {
-                    currentNumbIndicator = num;
-                    currentIndicator.addClass('active-indicator');
-                }
-
-                function indicatorClick(num) {
-                    if (num === currentNumbIndicator) {
-                        return;
-                    }
-                    console.log('indicator click: ' + num);
-
-                    if (num > currentNumbIndicator) {
-                        nextSlide();
-                    } else {
-                        prevSlide();
-                    }
-                    currentIndicator.removeClass('active-indicator');
-                    currentIndicator = $(indicatorControls).children().eq(num);
-                    setActiveIndicator(num);
-                }
-
-                function setupListeners() {
-                    for (const sliderControl of sliderControls) {
-                        sliderControl.addEventListener('click', controlClick);
-                    }
-                }
-
-                init();
-                setupListeners();
-
-
-                let waitForFinalEvent = (function () {
-                    let timers = {};
-                    return function (callback, ms, uniqueId) {
-                        if (!uniqueId) {
-                            uniqueId = "Don't call this twice without a uniqueId";
-                        }
-                        if (timers[uniqueId]) {
-                            clearTimeout(timers[uniqueId]);
-                        }
-                        timers[uniqueId] = setTimeout(callback, ms);
-                    };
-                })();
-
-                $(window).resize(function () {
-                    waitForFinalEvent(function () {
-                        init();
-                    }, 500, "some unique string");
-                });
+                return 5;
             }
 
-            this.each(function () {
-                main($(this));
+            function init() {
+                let countElement = options.countElement;
+                let widthOfContainer = container.width();
+                let slideItemWidth = widthOfContainer / countElement;
+                let slideItemHeight = sliderItems[0].clientHeight;
+
+                if (indicatorControls.length === 0) {
+                    container.css('height', slideItemHeight + 100);
+                } else {
+                    slideItemWidth = widthOfContainer / getCountElementBySizeWin();
+                    indicatorsCount = sliderItemsCount / getCountElementBySizeWin();
+                    $(indicatorControls).empty();
+
+                    for (let i = 0; i < indicatorsCount; i++) {
+                        const indicator = document.createElement("div");
+                        indicator.classList.add('indicator');
+                        $(indicatorControls).append(indicator);
+                        indicator.addEventListener("click", indicatorClick.bind(null, i), false);
+                    }
+                    currentIndicator = $(indicatorControls).children().first();
+                    setActiveIndicator(0);
+                }
+                sliderItemsContainer.css('width', slideItemWidth * sliderItemsCount);
+                sliderItems.css('width', slideItemWidth);
+            }
+
+            function nextSlide() {
+                sliderItemsContainer.animate(
+                    {'left': 0},
+                    500,
+                    moveNextCallback()
+                );
+            }
+
+            function moveNextCallback() {
+                sliderItemsContainer.children(':nth-last-child(-n+' + options.slideScroll + ')').prependTo(sliderItemsContainer);
+                sliderItemsContainer.css('left', -widthOfItem * options.slideScroll);
+            }
+
+            function prevSlide() {
+                sliderItemsContainer.animate(
+                    {'left': -widthOfItem * options.slideScroll},
+                    // {'left': 0},// if countElement: 4
+                    500,
+                    movePrevCallback()
+                );
+            }
+
+            function movePrevCallback() {
+                sliderItemsContainer.children(':nth-child(-n+' + options.slideScroll + ')').appendTo(sliderItemsContainer);
+                // sliderItemsContainer.css('left', widthOfItem * options.slideScroll); // if countElement: 4
+                sliderItemsContainer.css('left', 0);
+            }
+
+            let controlClick = function (e) {
+                if (e.target.classList.contains('arrow-left')) {
+                    e.preventDefault();
+                    prevSlide();
+                }
+                if (e.target.classList.contains('arrow-right')) {
+                    e.preventDefault();
+                    nextSlide();
+                }
+            };
+
+            function setActiveIndicator(num) {
+                currentNumbIndicator = num;
+                currentIndicator.addClass('active-indicator');
+            }
+
+            function indicatorClick(num) {
+                if (num === currentNumbIndicator) {
+                    return;
+                }
+                console.log('indicator click: ' + num);
+
+                if (num > currentNumbIndicator) {
+                    nextSlide();
+                } else {
+                    prevSlide();
+                }
+                currentIndicator.removeClass('active-indicator');
+                currentIndicator = $(indicatorControls).children().eq(num);
+                setActiveIndicator(num);
+            }
+
+            function setupListeners() {
+                for (const sliderControl of sliderControls) {
+                    sliderControl.addEventListener('click', controlClick);
+                }
+            }
+
+            init();
+            setupListeners();
+
+
+            let waitForFinalEvent = (function () {
+                let timers = {};
+                return function (callback, ms, uniqueId) {
+                    if (!uniqueId) {
+                        uniqueId = "Don't call this twice without a uniqueId";
+                    }
+                    if (timers[uniqueId]) {
+                        clearTimeout(timers[uniqueId]);
+                    }
+                    timers[uniqueId] = setTimeout(callback, ms);
+                };
+            })();
+
+            $(window).resize(function () {
+                waitForFinalEvent(function () {
+                    init();
+                }, 500, "some unique string");
             });
-            return this;
+            // }
+            //
+            // this.each(function () {
+            //     main($(this));
+            // });
+            // return this;
         }
     }
 )(jQuery);
